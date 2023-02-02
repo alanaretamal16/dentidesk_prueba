991db1a0ed7ccd5c3e036ecba085e0e88737ad8d
alana.retamal <alanageorgina24@gmail.com>
2023-02-02 14:39:03 -0300

    si22

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Felicidades, estas conectado!') }}
                 </div>
             </div>
         </div>
