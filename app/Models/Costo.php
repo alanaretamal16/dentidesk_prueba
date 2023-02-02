fe22602ac0b0f97ae4796db1e15befb549c50bfa
alana.retamal <alanageorgina24@gmail.com>
2023-02-02 13:35:15 -0300

    cambios2

---
+++ app/Models/Costo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Costo extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id','tratamiento_id','descripcion','valor_unitario','cantidad'];
+}
