2b7237c2e3622ee8bfaf2010af4fd245becb6d6f
alana.retamal <alanageorgina24@gmail.com>
2023-02-02 14:25:04 -0300

    diseño6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,8 @@
                 </div>
             @endif
 
-            
+            <link href="https://img.freepik.com/fotos-premium/espejo-dentista-diente-grande-clinica-dentista-fondo_488220-9369.jpg?w=740" rel="stylesheet">
+
 
                
                      
