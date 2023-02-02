17b896fb01da10fee5f4ea40e0be670d5ef7c3c3
alana.retamal <alanageorgina24@gmail.com>
2023-02-02 14:42:12 -0300

    diseñando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,18 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     {{ __('Felicidades, estas conectado!') }}
+                    <div class="dropdown-menu">
+                        <span class="dropdown-item-text">Dropdown item text</span>
+                        <a class="dropdown-item" href="#">Inicio</a>
+                        <a class="dropdown-item" href="#">Trabajos</a>
+                        <a class="dropdown-item" href="#">Quien soy</a>
+                    </div>
                 </div>
             </div>
         </div>
